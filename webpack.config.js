e0e62153496462f8c83e538bd48722d975d09c09
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-18 16:45:59 +1100

    вариации "scripts" для package.json и пакет cross-env

--- webpack.config.js
+++ webpack.config.js
@@ -45,3 +45,47 @@ module.exports = {
         maxEntrypointSize: 200000, 
     },
 };
+
+
+
+/*Иные варианты "scripts" в package.json:
+
+"scripts": {
+"start": "webpack serve",
+"build-dev": "webpack",
+"build-prod": "webpack --node-env=production",
+"clear": "rd /s /q dist || rimraf dist"
+}
+
+или 
+
+"scripts": {
+"start": "webpack-dev-server --mode development --open",
+"dev": "webpack --mode development",
+"build": "webpack --mode production",
+"watch": "webpack --mode development --watch"
+}
+
+или 
+
+"scripts": {
+"start": "set NODE_ENV=development&&webpack serve",
+"dev": "set NODE_ENV=development&&webpack",
+"build": "set NODE_ENV=production&&webpack",
+}
+
+или
+"scripts": {
+"start": "cross-env NODE_ENV=development webpack-dev-server --mode development --open",
+"dev": "cross-env NODE_ENV=development webpack --mode development",
+"build": "cross-env NODE_ENV=production webpack --mode production",
+"watch": "cross-env NODE_ENV=development webpack --mode development --watch"
+}
+
+Потребуются и утилиты: npm install rimraf --save-dev и npm install cross-env --save-dev;
+
+Касательно "build", может быть и такая вариация - 
+"build": "webpack --config webpack.config.js"
+
+
+*/
