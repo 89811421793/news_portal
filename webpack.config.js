9cd7897f8905bfd534a4914b5e1752845ec2d356
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-19 13:41:20 +1100

    formatted

--- webpack.config.js
+++ webpack.config.js
@@ -1,136 +1,137 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
-const ENV = process.env.NODE_ENV === "production" ? "production" : "development";
-const devMode = ENV === 'development';
-const target = devMode ? 'web' : "browserslist";
-const devtool = devMode ? 'source-map' : undefined;
+const ENV =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+const devMode = ENV === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
 
 module.exports = {
-    mode: ENV,
-    target: target,
-    devtool: devtool,
-    entry: [
-        'core-js/stable',
-        'regenerator-runtime/runtime',
-        path.resolve(__dirname, "src", "index.js")
-    ],
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        assetModuleFilename: 'assets/[hash][ext]',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.hbs$/,
-                loader: 'handlebars-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.(png|jpe?g|gif|webp|svg)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource',
-            },
+  mode: ENV,
+  target: target,
+  devtool: devtool,
+  entry: [
+    "core-js/stable",
+    "regenerator-runtime/runtime",
+    path.resolve(__dirname, "src", "index.js"),
+  ],
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "assets/[hash][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.hbs$/,
+        loader: "handlebars-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/mainpage.hbs',
-            filename: 'index.html',
-            inject: true,
-            minify: {
-                removeComments: false,
-                collapseWhitespace: true,
-                minifyCSS: true,
-                minifyJS: true,
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/pages/articlepage.hbs',
-            filename: 'article.html',
-            minify: {
-                removeComments: false,
-                collapseWhitespace: true,
-                minifyCSS: true,
-                minifyJS: true,
-            }
-        }),
-        new BundleAnalyzerPlugin({
-            analyzerMode: devMode ? 'server' : 'disabled',
-            openAnalyzer: false,
-        }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp|svg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
     ],
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            minSize: 20000, // минимальный размер для сплиттинга
-            maxSize: 70000, // максимальный размер для чанков
-            minChunks: 1, // минимальное количество использований для создания чанка
-            maxAsyncRequests: 30, // максимальное количество асинхронных запросов
-            maxInitialRequests: 30, // максимальное количество начальных запросов
-            automaticNameDelimiter: '~',
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                    chunks: 'all',
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true,
-                },
-            },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/mainpage.hbs",
+      filename: "index.html",
+      inject: true,
+      minify: {
+        removeComments: false,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/articlepage.hbs",
+      filename: "article.html",
+      minify: {
+        removeComments: false,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: devMode ? "server" : "disabled",
+      openAnalyzer: false,
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000, // минимальный размер для сплиттинга
+      maxSize: 70000, // максимальный размер для чанков
+      minChunks: 1, // минимальное количество использований для создания чанка
+      maxAsyncRequests: 30, // максимальное количество асинхронных запросов
+      maxInitialRequests: 30, // максимальное количество начальных запросов
+      automaticNameDelimiter: "~",
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all",
         },
-        minimize: true,
-        minimizer: [new TerserWebpackPlugin()],
-    },
-    cache: {
-        type: 'filesystem',
-    },
-    performance: {
-        hints: false
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
         },
-        compress: true,
-        port: 9000,
-        historyApiFallback: true,
-        hot: false,
-        open: true,
-        client: {
-            overlay: {
-                errors: true,
-                warnings: false,
-            },
-        },
+      },
     },
+    minimize: true,
+    minimizer: [new TerserWebpackPlugin()],
+  },
+  cache: {
+    type: "filesystem",
+  },
+  performance: {
+    hints: false,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+    hot: false,
+    open: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
 };
 
 /*
@@ -161,6 +162,7 @@ module.exports = {
 }
 
 или
+
 "scripts": {
 "start": "cross-env NODE_ENV=development webpack-dev-server --mode development --open",
 "dev": "cross-env NODE_ENV=development webpack --mode development",
@@ -176,7 +178,7 @@ module.exports = {
 
 /*
 vendor.js: Этот файл обычно содержит сторонние библиотеки и зависимости, такие как jQuery, Bootstrap и другие.
-Это позволяет разделить код вашего приложения и сторонние библиотеки, что может улучшить кэширование, так как
+Это позволяет разделить код приложения от сторонних библиотек, что может улучшить кэширование, так как
 изменения в коде не будут затрагивать этот файл, и пользователи смогут использовать кэшированную версию.
 
 manifest.js: Этот файл содержит метаданные о модулях и их зависимости. Он помогает Webpack правильно загружать
