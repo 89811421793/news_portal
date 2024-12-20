7864abb822849f2952e508e85c9e8230dfae81eb
Maksim Dorogoi <monarch2013@mail.ru>
2024-12-20 16:41:13 +1100

    Container из Bootstrap

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap'; // если будем использовать и JS функциональность Bootstrap
 
 $(document).ready(function() {
-    alert('Hello'); 
+   
     $('h1').addClass('text-danger'); 
-    $('h2').attr('style', 'color: red;');
-    $('h3').css('color', 'crimson'); 
+    
 });
